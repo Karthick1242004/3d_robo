8228c5e1a6f3c1aeec182d3ce0b4558a0f3af670
Karthick1242004 <karthick1242004@gmail.com>
2024-07-06 21:33:00 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+import './App.css';
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/0AF0j4QMD8ywCTqP/scene.splinecode" />
+  );
+}
+
