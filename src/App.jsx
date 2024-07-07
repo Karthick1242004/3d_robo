0cd6f1b1fa03ee7fd8487a23fd81bc712aba3e75
Karthick1242004 <karthick1242004@gmail.com>
2024-07-07 15:37:16 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import Spline from '@splinetool/react-spline';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Splines from './components/Splines'
+import Splines from './components/Splinescene/Splines';
+import Navbar from './components/Navbar/Navbar';
+import Loader from './components/Loader/Loader';
+
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Change this to the actual loading time of your app
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className='spline'>
-     <Splines/>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className='navb'>
+            <Navbar />
+          </div>
+          <div className='sp1'>
+            <Splines />
+            <div className='main'>
+              <h1>Hello Everyone</h1>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
